e54f5ad4da5dbe9b423ce4cdc6e4de124a414fee
yasbiotec <yasminym.czm@gmail.com>
2025-11-30 20:18:57 -0300

    Updates

---
+++ _next/static/8B6I_8GWJJG86O0bA1-t0/_buildManifest.js
@@ -0,0 +1,28 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/9e9b8d337297fa7f.js"
+  ],
+  "/_error": [
+    "static/chunks/6f93f266acda4d4e.js"
+  ],
+  "/programa": [
+    "static/chunks/2dab08169dd3c2ed.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hemohelp//_next/:path+",
+        "destination": "/hemohelp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/hello",
+    "/programa"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/6f93f266acda4d4e.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/e6596319c29775da.js",
+  "static/chunks/9d66f3ceebf6c665.js",
+  "static/chunks/6d7d8433ecd5f46d.js",
+  "static/chunks/turbopack-8339f9f214afdc28.js"
+])
