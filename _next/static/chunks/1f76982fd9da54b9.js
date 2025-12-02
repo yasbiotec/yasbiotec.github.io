2707e925ad355690d7bdc32d47d8b20daf79f145
yasbiotec <yasminym.czm@gmail.com>
2025-12-02 17:20:12 -0300

    Updates

--- _next/static/chunks/1f76982fd9da54b9.js
+++ _next/static/chunks/1f76982fd9da54b9.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/80a5b2434ef60d3c.js",
   "static/chunks/ee4f4511a9c1577c.js",
   "static/chunks/81ce303d9f1914d3.js",
-  "static/chunks/5af32d58fc8f441e.css",
-  "static/chunks/turbopack-70d4d1964c818921.js"
+  "static/chunks/4fa1bbdab4965329.css",
+  "static/chunks/turbopack-7803dda40f7190e4.js"
 ])
